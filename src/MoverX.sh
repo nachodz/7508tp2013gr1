86d28da79de57d5dcdfe226d742bf2bf05b17643
lauta1608@gmail.com <lauta1608@gmail.com@75cb959a-d12e-ab83-5b63-e5bed7fed48e>
2013-04-19 15:36:18 +0000

    Actualización del script MoverX.sh. Falta el número de duplicado. PROBAR!

--- src/MoverX.sh
+++ src/MoverX.sh
@@ -11,23 +11,34 @@ function MoverX {
    origen = $1
    destino = $2
    comando = $3
+   
+   carpetaOrigen = ${origen%/*}
+   archivoOrigen = ${origen##*/}
 
-   # 1.1 Verifico si origen y destino son iguales
-   if [ $origen = $destino ] 
+   # 1.1 Verifico si la carpeta de origen y de destino son iguales
+   if [ $carpetaOrigen = $destino ] 
    then
       return 1
    fi
 
-   # 1.2 Verifico que existan origen y destino
-   if [ ! -f $origen  ] || [ ! -f $destino ]
+   # 1.2 Verifico que existan el archivo origen y la carpeta destino
+   if [ ! -f $origen  ] || [ ! -d $destino ]
    then
       return 1
    fi
 
    # 1.3 Verifico si es un archivo duplicado
-   if [ -f $destino ] 
+   if [ -f $destino$archivo ] 
    then
-      #Manejo el duplicado ...
+      # Verifico si existe la carpeta dup, si no, la creo
+	  if [ ! -d "dup" ]; then
+	     mkdir dup
+      fi
+	  
+	  # Falta generar el número de control
+	  
+	  #Copio el archivo a la carpeta de duplicados
+	  mv $origen dup/$destino
    else 
       mv $origen $destino
    fi
