c54b14dfa404696bb26b2837c909f93cecba862e
lauta1608@gmail.com <lauta1608@gmail.com@75cb959a-d12e-ab83-5b63-e5bed7fed48e>
2013-04-22 16:00:06 +0000

    Corregido el script de MoverX.sh

--- trunk/src/MoverX.sh
+++ trunk/src/MoverX.sh
@@ -1,45 +1,52 @@
 #! /bin/bash
 
-function MoverX {
+function MoverX () {
    # Si no se pasan los 3 parametros solicitados, es un error
-   if [ $# -eq 3] 
+   if [ $# -eq 4 ] 
    then
       return 1
    fi
 
    # Tomo los parametros
-   origen = $1
-   destino = $2
-   comando = $3
+   origen=$1
+   destino=$2
+   comando=$3
    
-   carpetaOrigen = ${origen%/*}
-   archivoOrigen = ${origen##*/}
-
+   carpetaOrigen=${origen%/*}
+   archivo=${origen##*/}
+   
    # 1.1 Verifico si la carpeta de origen y de destino son iguales
    if [ $carpetaOrigen = $destino ] 
    then
+      echo "Origen y destino iguales"
       return 1
    fi
 
    # 1.2 Verifico que existan el archivo origen y la carpeta destino
    if [ ! -f $origen  ] || [ ! -d $destino ]
    then
+      echo "No existe origen o destino"
       return 1
    fi
 
    # 1.3 Verifico si es un archivo duplicado
-   if [ -f $destino$archivo ] 
+   echo "$destino/$archivo"
+   if [ -f "$destino/$archivo" ] 
    then
       # Verifico si existe la carpeta dup, si no, la creo
-	  if [ ! -d "dup" ]; then
-	     mkdir dup
+	  if [ ! -d "$destino/dup" ]; then
+	     mkdir "$destino/dup"
       fi
 	  
 	  # Falta generar el número de control
 	  
 	  #Copio el archivo a la carpeta de duplicados
-	  mv $origen dup/$destino
-   else 
-      mv $origen $destino
+      echo "Duplicado"
+	  cp $origen "$destino/dup/$archivo"
+	  return 1
    fi
+     
+   echo "Todo piola"
+   cp $origen $destino
+   return 0
 }
