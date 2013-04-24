61df4d17170e7158f474f2990a4cfaabbc182c18
lauta1608@gmail.com <lauta1608@gmail.com@75cb959a-d12e-ab83-5b63-e5bed7fed48e>
2013-04-24 16:49:59 +0000

    Primera aproximación (?) a la función glogx

--- src/GlogX.sh
+++ src/GlogX.sh
@@ -1,1 +1,29 @@
- 
+#! /bin/bash
+
+# 4to parametro: nombre del archivo de log sin extension
+function GlogX {
+   # Si no se pasan los 4 parametros solicitados, es un error
+   if [ $# -eq 5 ]
+   then
+      return 1
+   fi
+   
+   comando=$1
+   tipoMensaje=$2
+   mensaje=$3
+   archivoLog=$4
+   directorioLog=$LOGDIR
+   
+   fecha=$(date)
+   usuario=$(whoami)
+   
+   if [ comando = "InstalarX.sh" ]
+   then
+      directorioLog=$CONFDIR
+      # Recortar log
+   fi
+   
+   # Escribo el log
+   # When Who Where What Why
+   echo $fecha $usuario $comando $tipoMensaje $mensaje >> $directorioLog/$archivoLog.$LOGEXT   
+}
