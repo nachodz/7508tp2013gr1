9a62d5f679ea31046e89cbf526a8df237b245920
lauta1608@gmail.com <lauta1608@gmail.com@75cb959a-d12e-ab83-5b63-e5bed7fed48e>
2013-04-25 05:13:21 +0000

    GlogX terminado

--- trunk/src/GlogX.sh
+++ trunk/src/GlogX.sh
@@ -2,8 +2,17 @@
 
 # 4to parametro: nombre del archivo de log sin extension
 function GlogX {
+   # Cantidad de lineas a dejar
+   nroLineas=50
+
    # Si no se pasan los 4 parametros solicitados, es un error
    if [ $# -eq 5 ]
+   then
+      return 1
+   fi
+   
+   # Chequeo si están seteadas las variables de ambiente
+   if [ -z $LOGEXT ] && [ -z $LOGDIR ] && [ -z $LOGSIZE ] && [ -z $CONFDIR ]
    then
       return 1
    fi
@@ -20,10 +29,16 @@ function GlogX {
    if [ comando = "InstalarX.sh" ]
    then
       directorioLog=$CONFDIR
-      # Recortar log
+   else
+      # Si no es el log de instalacion, veo el tamaño
+      if [ -f $directorioLog/$archivoLog.$LOGEXT ] && [ `du -k $directorioLog/$archivoLog.$LOGEXT | awk '{ print $1 }'` -gt $LOGSIZE ]
+      then   
+         tail -50 $directorioLog/$archivoLog.$LOGEXT > $directorioLog/$archivoLog.$LOGEXT
+         echo $fecha $usuario $comando $tipoMensaje "Log Excedido" >> $directorioLog/$archivoLog.$LOGEXT
+      fi
    fi
    
    # Escribo el log
    # When Who Where What Why
-   echo $fecha $usuario $comando $tipoMensaje $mensaje >> $directorioLog/$archivoLog.$LOGEXT   
+   echo $fecha $usuario $comando $tipoMensaje $mensaje >> $directorioLog/$archivoLog.$LOGEXT
 }
