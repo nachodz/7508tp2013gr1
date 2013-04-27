3cdc9c0a7058c3c0d6dbc402bd084d9a6c44c879
damfinkel@gmail.com <damfinkel@gmail.com@75cb959a-d12e-ab83-5b63-e5bed7fed48e>
2013-04-27 01:49:21 +0000

    subscript de DetectaX.sh

---
+++ src/valPais.sh
@@ -0,0 +1,65 @@
+#!/bin/bash
+
+pais=$1
+sistema=$2
+archPath=$PWD/$3
+paisEncontrado=false
+sistemaEncontrado=false
+combinacionEncontrada=false
+for i in $(sed "s%^\([A-Z]-[0-9]\)\(-.*$\)%\1%g" $3)
+do
+	codPais=$(echo "$i" | cut -s -f1 -d'-' )
+	codSistema=$(echo "$i" | cut -s -f2 -d'-' )
+
+	if [[ "$pais" = "$codPais" ]] && [[ "$sistema" = "$codSistema" ]]
+	then
+		combinacionEncontrada=true
+		break
+	fi
+	
+	if [[ "$paisEncontrado" = false ]] && [[ "$pais" = "$codPais" ]]
+	then
+		paisEncontrado=true
+	fi
+
+	if [[ "$sistemaEncontrado" = false ]] && [[ "$sistema" = "$codSistema" ]]
+	then
+		sistemaEncontrado=true
+	fi
+
+done
+
+if [[ "$combinacionEncontrada" = true ]]
+then
+	echo "Valido"
+	exit 0
+fi
+
+if [[ "$paisEncontrado" = false ]] && [[ "$sistemaEncontrado" = false ]] 
+then
+	echo "No existe ni sistema ni pais"
+	exit 1
+fi
+
+if [[ "$paisEncontrado" = false ]]
+then
+	echo "No existe pais"
+	exit -2
+fi
+
+if [[ "$sistemaEncontrado" = false ]]
+then
+	echo "No existe sistema"
+	exit 3
+fi
+
+if [[ "$paisEncontrado" = true ]] && [[ "$sistemaEncontrado" = true ]] 
+then
+	echo "No existe combinacion"
+	#exit 4
+fi
+
+
+
+
+
