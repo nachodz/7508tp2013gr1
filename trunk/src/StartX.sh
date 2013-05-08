bb4fb1ad9dd46c40fcdfffd5f6f676fcfcc98265
ibayetto@gmail.com <ibayetto@gmail.com@75cb959a-d12e-ab83-5b63-e5bed7fed48e>
2013-05-08 00:30:21 +0000

    Subo todo

--- trunk/src/StartX.sh
+++ trunk/src/StartX.sh
@@ -4,6 +4,8 @@
 # Parametro 2: comando a ejecutar
 function StartX {
 
+   source GlogX.sh; 
+
    if [ $# -eq 3 ]
    then
       return 1
@@ -13,15 +15,22 @@ function StartX {
    comandoAEjecutar=$2
 
    nombreComando=$( echo "$comandoAEjecutar" | cut -f1 -d" ")
-   
+      
    var=`ps -fea | grep -v "grep" | grep -v "StartX" | grep "$nombreComando" | wc -l`
-   
    if [ "$var" -ne 0 ]
    then
+      GlogX "$comando.sh" "SE" "StartX.sh encuentra un proceso $nombreComando en ejecución" "$comando"
       return 1
    fi
 
-   $comandoAEjecutar &
+   GlogX "$comando.sh" "I" "StartX.sh invoca a $nombreComando" "$comando"
+
+   if [ $nombreComando == "DetectaX.sh" ]; then
+	   $comandoAEjecutar &	
+   else
+	   $comandoAEjecutar
+   fi
+ 
    return $!
    
 }
